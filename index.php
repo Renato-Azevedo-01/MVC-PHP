761f9060faa2d973b558971097187a72d3e11818
Renato Azevedo <renato.azevedo0001@gmail.com>
2022-11-15 12:53:32 -0300

    MVC - Inseri o Model

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     require __DIR__ . '/vendor/autoload.php' ;
 
+    #use \App\Controller\Pages\Linguagens;
     use \App\Controller\Pages\Home;
-
+    #echo Linguagens::getLinguagens();
     echo Home::getHome();
+
 ?>
